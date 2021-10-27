418f6ad2a24a896bc280732ce18669be9e3933d8
Adam Turner <hello@adamrichardturner.dev>
2021-10-27 04:57:43 +0100

    Completed Codewars challenge to remove all vowels from a string

---
+++ disemvowel.py
@@ -0,0 +1,16 @@
+import re
+
+def disemvowel(string_):
+    """
+    Write a function that takes a string and return a new string
+    with all the vowels removed.
+    For example, the string "This website is for losers LOL!" would 
+    become "Ths wbst s fr lsrs LL!".
+    """
+    # Use regex to split the string into a list of chars without the 
+    # vowels.
+    noVowel = re.split(r'a|A|e|E|i|I|o|O|u|U', string_)
+    # Join the char list as a string and return it.
+    return ''.join(noVowel)
+
+print(disemvowel("This website is for losers LOL!"))
