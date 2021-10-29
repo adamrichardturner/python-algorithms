fab08fcf745c339adabbbfafcae783fe00a958ea
Adam Turner <hello@adamrichardturner.dev>
2021-10-29 21:00:48 +0100

    Modified mistake and solved algo challenge

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -14,28 +14,36 @@ def valid_parentheses(string):
     not contain any parentheses at all. Do not treat other forms of brackets as 
     parentheses (e.g. [], {}, <>).
     """
-
-    # Empty Stack
+    # We store our parenthesis in order, stripped of anything else in the string.
+    paren = []
+    # We keep our stack here, it should be empty if the paranthesis are valid.
     stack = []
+    # Appending only parenthesis to the paren variable.
     for char in string:
+        if '(' in char or ')' in char:
+            paren.append(char)
+    for char in paren:
         # If open parenthesis, push ontop the stack.
         if char == '(':
             stack.append(char)
         else:
-        # If other, check if stack is empty return False
+        # If other, check if stack is empty return False.
             if not stack:
                 return False
-            # If not empty, store last index of stack in top
+            # If not empty, store last index of stack in top.
             else:
                 top = stack[-1]
             # If the next char is closing and top is opening, pop
             # last char off the stack.
                 if char == ')' and top == '(':
                     stack.pop()
-    # If stack is empty, we have valid parenthesis, otherwise False.
+    # Final test, check if stack is empty or not. If empty return True, else False.
     if not stack:
         return True
     else:
         return False
 
-print(valid_parentheses("((hello()))"))
+print(valid_parentheses("()")) # True
+print(valid_parentheses(")(()))")) # False
+print(valid_parentheses("(")) # False
+print(valid_parentheses("(())((()())())")) # True
