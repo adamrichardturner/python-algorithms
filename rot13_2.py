c4f02ee8f97e799163522e0a4ad746b84a97d423
Adam Turner <hello@adamrichardturner.dev>
2021-11-07 23:40:57 +0000

    Modified to add comments

--- rot13_2.py
+++ rot13_2.py
@@ -8,16 +8,20 @@ def rot13(message):
     rot13("EBG13 rknzcyr.") == "ROT13 example.";
     rot13("This is my first ROT13 excercise!" == "Guvf vf zl svefg EBG13 rkprepvfr!"
     """
-
+    # We store the alphabet in lowercase in alphaLower and in uppercase in alphaUpper.
     alphaLower = "abcdefghijklmnopqrstuvwxyz"
     alphaUpper = alphaLower.upper()
+    # We store our final representation of rot13 here.
     rot13 = []
+    # For each char (i) in message, if it is upper append the upper case letter shifted
+    # 13 places down the alphabet. Same process for lower.
     for i in message:
         if i.isupper():
             rot13.append(alphaUpper[alphaUpper.index(i) - 13])
         elif i.islower():
             rot13.append(alphaLower[alphaLower.index(i) - 13])
         else:
+        # If we have anything other than a letter, we simply append it as it is. 
             rot13.append(i)
     return ''.join(rot13)
 
