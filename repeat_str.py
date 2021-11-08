519c9a263d91c58ed48fee2f946898c4ff3bc2fc
Adam Turner <hello@adamrichardturner.dev>
2021-11-08 21:43:14 +0000

    Completed 8kyu Kata

---
+++ repeat_str.py
@@ -0,0 +1,8 @@
+def repeat_str(repeat, string):
+    """
+    Write a function called repeatStr which repeats the given string string exactly n times.
+    """
+    return string * repeat
+
+print(repeat_str(6, "I"))
+print(repeat_str(5, "Hello"))
