ba5021b8c330adc976039f6cd0bd03ea2597ae88
Adam Turner <hello@adamrichardturner.dev>
2021-12-18 22:20:58 +0000

    Completed kata

---
+++ count_inversions.py
@@ -0,0 +1,26 @@
+def count_inversions(array):
+    """
+    Array inversion indicates how far the array is from being sorted.
+
+    Inversions are pairs of elements in array that are out of order.
+
+    Examples
+    [1, 2, 3, 4]  =>  0 inversions
+    [1, 3, 2, 4]  =>  1 inversion: 2 and 3
+    [4, 1, 2, 3]  =>  3 inversions: 4 and 1, 4 and 2, 4 and 3
+    [4, 3, 2, 1]  =>  6 inversions: 4 and 3, 4 and 2, 4 and 1, 3 and 2, 3 and 1, 2 and 1
+    """
+    inversions = 0
+    sortedList = []
+    while array:
+        min = array[0]
+        for num in array:
+            if num < min:
+                inversions += 1
+                min = num
+        sortedList.append(min)
+        array.remove(min)
+    return inversions
+    
+
+print(count_inversions([6,5,4,3,3,3,3,2,1])) # Returns 24, should return 30
