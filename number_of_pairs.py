a948d5b517ebe6c637bb5491da87b20f2c1bd71b
Adam Turner <hello@adamrichardturner.dev>
2022-01-01 23:13:07 +0000

    adding challenges

--- number_of_pairs.py
+++ number_of_pairs.py
@@ -21,4 +21,4 @@ def number_of_pairs(gloves):
     return countPairs
 
 print(number_of_pairs(["gray","black","purple","purple","gray","black"])) # 3
-print(number_of_pairs(["red","green","blue","blue","red","green","red","red","red"])) # 3
+print(number_of_pairs(["red","green","blue","blue","red","green","red","red","red"])) # 4
